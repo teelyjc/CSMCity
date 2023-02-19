3eb91604c4479f68c9a54fd64633c6b28c0c0834
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-02-19 17:56:45 +0700

    feat: add auth page

---
+++ services/client/src/pages/auth/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import { LayoutComponent } from '@component/LayoutComponent';
+import { SignUpForm } from '@component/auth/SignUpForm';
+
+
+export const SignUpPage = () => {
+	return (
+		<LayoutComponent>
+			<SignUpForm />
+		</LayoutComponent>
+	)
+}
