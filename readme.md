ccb312df9ba7c7faecbcb2d5100486883d38dc14
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-02-24 21:52:55 +0700

    set to legacy

--- readme.md
+++
@@ -1,6 +0,0 @@
-# CSMCity - Project
-
-
-> This project using **[turborepo](https://turbo.build/repo)** and **[pnpm](https://pnpm.io/)**.
-
-This project is under construction.
--- services/server/src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Express {
-    export interface User {
-      _id: string;
-      username: string;
-      password: string;
-      email: string;
-      salt: string
-    }
-  }
